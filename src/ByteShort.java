0e7c964a5c8f07a7de0c9039caaeaa7499327c8a
N3KK1D <39355656+Zolton@users.noreply.github.com>
2020-05-12 17:01:38 -0400

    Done for today

--- src/ByteShort.java
+++ src/ByteShort.java
@@ -5,5 +5,10 @@ public class ByteShort {
         int minimumValue = Integer.MIN_VALUE;
         int maxValue = Integer.MAX_VALUE;
         System.out.println("test " + minimumValue + " test2 " + maxValue);
+        boolean tests = false;
+        if (!tests)
+            System.out.println("success");
+        boolean test2 = true;
+        test2 ? System.out.println("True") : System.out.println("false");
     }
 }
