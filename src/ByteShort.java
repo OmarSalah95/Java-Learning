d577bc2bfac84a9ade269593912c64981734a8c3
N3KK1D <39355656+Zolton@users.noreply.github.com>
2020-05-12 10:39:23 -0400

    Java primitive data types

---
+++ src/ByteShort.java
@@ -0,0 +1,9 @@
+package src;
+
+public class ByteShort {
+    public static void main (String[] args) {
+        int minimumValue = Integer.MIN_VALUE;
+        int maxValue = Integer.MAX_VALUE;
+        System.out.println("test " + minimumValue + " test2 " + maxValue);
+    }
+}
