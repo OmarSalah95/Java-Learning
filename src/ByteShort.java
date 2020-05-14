5b324c4f7829535cd8c3e3a929c48d12858f6f3f
N3KK1D <39355656+Zolton@users.noreply.github.com>
2020-05-14 19:54:46 -0400

    more sscanner

--- src/ByteShort.java
+++ src/ByteShort.java
@@ -4,13 +4,21 @@ import java.util.Scanner;
 
 public class ByteShort {
     public static void main(String[] args) {
-        System.out.println("Enter name: ");
+        
         // User input
         Scanner scannerTest = new Scanner(System.in);
+        System.out.println("Enter name: ");
         // Read the next line, capture it as a string called name
         String name = scannerTest.nextLine();
+
+        System.out.println("Enter DOB: ");
+        long age = scannerTest.nextLong();
+        age = 2020-age;
+
         System.out.println("Your name is : " + name);
+        System.out.println("Your age is: " + age);
         // Release the memory
         scannerTest.close();
-}
+
+    }
 }
