382bef5c97f8116a353293a7e7e15ae4eefb8dda
N3KK1D <39355656+Zolton@users.noreply.github.com>
2020-05-15 13:06:55 -0400

    Default settings for constructor

--- src/Car.java
+++ src/Car.java
@@ -9,78 +9,28 @@ public class Car {
     private String color;
     // Set a default if no info provided on class creation
     // calls the method that matches parameters given
-    public Car() {
-        this("Camry", 2, 4);
-        System.out.println("Empty constructor called");
-    }
+    // public Car() {
+    //     this("Camry", 2, 4);
+    //     System.out.println("Empty constructor called");
+    // }
     // Constructor
 
     // Every time a new class of this is created, do this:
     // way to initialize the class at time of creation,
     // instead of using 20 setters every time
-    public Car(String model, int doors, int wheels) {
+    public Car(String model, int doors, int wheels, String engine, String color) {
+        this.engine = engine;
+        this.color = color;
         this.model = model;
         this.doors = doors;
         this.wheels = wheels;
-        System.out.println("New car has been made");
+        System.out.println("New car has been made: " + model + doors + wheels + engine + color);
     }
 
-    public String getColor() {
-        return color;
+    public Car(String model) {
+        this(model, 2, 4, "4 cylinder", "red");
+        System.out.println("Default settings applied");
     }
 
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public String getEngine() {
-        return engine;
-    }
-
-    public void setEngine(String engine) {
-        this.engine = engine;
-    }
-
-    public Car(String engine, String color) {
-        this.setEngine(engine);
-        this.setColor(color);
-        System.out.println("only 2 ");
-    }
-
-    
-
-    public int getDoors() {
-        return doors;
-    }
-
-    public void setDoors(int doors) {
-        this.doors = doors;
-    }
-
-    public int getWheels() {
-        return wheels;
-    }
-
-    public void setWheels(int wheels) {
-        this.wheels = wheels;
-    }
-
-    // setter - settting state/attributes of class
-    public void setModel(String model) {
-        // change input to lowercase, check if its what's expected below
-        String validateModel = model.toLowerCase();
-        //  Encapsulation - internal safety check to make sure only preapproved
-        //  state/attributes can be added to a class
-        if (validateModel.equals("carrera") || validateModel.equals("911")) {
-            this.model = model;
-        }
-        else {
-            this.model = "unknown";
-        }
-    }
-    // getter - retrieving state/attributes of class
-    public String getModel () {
-        return this.model;
-    }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,11 @@ package src;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test1");
-        Car porsche = new Car();
-        System.out.println("tests2");
+        Car porsche = new Car("Porsche");
         //Car holden = new Car();
-        System.out.println("Model is " + porsche.getModel());
-        System.out.println("WHeels are is " + porsche.getWheels());
-        System.out.println("Doors is " + porsche.getDoors());
+        // System.out.println("Model is " + porsche.getModel());
+        // System.out.println("WHeels are is " + porsche.getWheels());
+        // System.out.println("Doors is " + porsche.getDoors());
         // Car holden = new Car("6 cylinder", "red");
         // System.out.println("Model is " + holden.getModel());
         // System.out.println("WHeels are is " + holden.getWheels());
