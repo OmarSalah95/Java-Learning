e4a65da3e49803054f2c12609afd2738f979ac47
N3KK1D <39355656+Zolton@users.noreply.github.com>
2020-05-14 20:04:15 -0400

    nextLine() after EVERY number input

--- src/ByteShort.java
+++ src/ByteShort.java
@@ -7,16 +7,33 @@ public class ByteShort {
         
         // User input
         Scanner scannerTest = new Scanner(System.in);
+        
+        
+        System.out.println("Enter DOB: ");
+        long age = scannerTest.nextLong();
+        age = 2020-age;
+        scannerTest.nextLine();
+
         System.out.println("Enter name: ");
         // Read the next line, capture it as a string called name
         String name = scannerTest.nextLine();
 
-        System.out.println("Enter DOB: ");
-        long age = scannerTest.nextLong();
-        age = 2020-age;
+        System.out.println("enter a number");
+        int randomasdf = scannerTest.nextInt();
+        scannerTest.nextLine();
+        
+        // Must ALWAYS include nextLine after taking in a number!!
+
+        System.out.println("Enter another name: ");
+        // Read the next line, capture it as a string called name
+        String secondname = scannerTest.nextLine();
+
+        
 
         System.out.println("Your name is : " + name);
         System.out.println("Your age is: " + age);
+        System.out.println("number" + randomasdf);
+        System.out.println("second" + secondname);
         // Release the memory
         scannerTest.close();
 
