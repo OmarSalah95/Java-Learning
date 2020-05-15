c4502f539270f9a351d36230624dbdc09f412c2b
N3KK1D <39355656+Zolton@users.noreply.github.com>
2020-05-14 20:14:00 -0400

    Handling type errors from user input

--- src/ByteShort.java
+++ src/ByteShort.java
@@ -4,36 +4,43 @@ import java.util.Scanner;
 
 public class ByteShort {
     public static void main(String[] args) {
-        
+
         // User input
         Scanner scannerTest = new Scanner(System.in);
-        
-        
+
         System.out.println("Enter DOB: ");
-        long age = scannerTest.nextLong();
-        age = 2020-age;
-        scannerTest.nextLine();
+        // checks if input is an integer; if not, 
+        // it'll return false, which can be handled
 
-        System.out.println("Enter name: ");
-        // Read the next line, capture it as a string called name
-        String name = scannerTest.nextLine();
+        boolean hasNextInt = scannerTest.hasNextInt();
+        if (hasNextInt) {
+            long age = scannerTest.nextLong();
+            age = 2020 - age;
+            scannerTest.nextLine();
 
-        System.out.println("enter a number");
-        int randomasdf = scannerTest.nextInt();
-        scannerTest.nextLine();
-        
-        // Must ALWAYS include nextLine after taking in a number!!
+            System.out.println("Enter name: ");
+            // Read the next line, capture it as a string called name
+            String name = scannerTest.nextLine();
 
-        System.out.println("Enter another name: ");
-        // Read the next line, capture it as a string called name
-        String secondname = scannerTest.nextLine();
+            System.out.println("enter a number");
+            int randomasdf = scannerTest.nextInt();
+            scannerTest.nextLine();
 
-        
+            // Must ALWAYS include nextLine after taking in a number!!
 
-        System.out.println("Your name is : " + name);
+            System.out.println("Enter another name: ");
+            // Read the next line, capture it as a string called name
+            String secondname = scannerTest.nextLine();
+            System.out.println("Your name is : " + name);
         System.out.println("Your age is: " + age);
         System.out.println("number" + randomasdf);
         System.out.println("second" + secondname);
+        }
+        else {
+            System.out.println("invalid input");
+        }
+
+        
         // Release the memory
         scannerTest.close();
 
