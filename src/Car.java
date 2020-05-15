2fd6dc08c7e93cf006c2df24f3ab4e506e220fc0
N3KK1D <39355656+Zolton@users.noreply.github.com>
2020-05-15 12:43:12 -0400

    Class constructors

--- src/Car.java
+++ src/Car.java
@@ -7,6 +7,63 @@ public class Car {
     private String model;
     private String engine;
     private String color;
+    // Set a default if no info provided on class creation
+    // calls the method that matches parameters given
+    public Car() {
+        this("Camry", 2, 4);
+        System.out.println("Empty constructor called");
+    }
+    // Constructor
+
+    // Every time a new class of this is created, do this:
+    // way to initialize the class at time of creation,
+    // instead of using 20 setters every time
+    public Car(String model, int doors, int wheels) {
+        this.model = model;
+        this.doors = doors;
+        this.wheels = wheels;
+        System.out.println("New car has been made");
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    public Car(String engine, String color) {
+        this.setEngine(engine);
+        this.setColor(color);
+        System.out.println("only 2 ");
+    }
+
+    
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
 
     // setter - settting state/attributes of class
     public void setModel(String model) {
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,20 @@ package src;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("test1");
         Car porsche = new Car();
-        Car holden = new Car();
+        System.out.println("tests2");
+        //Car holden = new Car();
         System.out.println("Model is " + porsche.getModel());
-        porsche.setModel("Carrera");
-        System.out.println("Model is " + porsche.getModel());
+        System.out.println("WHeels are is " + porsche.getWheels());
+        System.out.println("Doors is " + porsche.getDoors());
+        // Car holden = new Car("6 cylinder", "red");
+        // System.out.println("Model is " + holden.getModel());
+        // System.out.println("WHeels are is " + holden.getWheels());
+        // System.out.println("Doors is " + holden.getDoors());
+        // System.out.println("Doors is " + holden.getEngine());
+        // System.out.println("Doors is " + holden.getColor());
+
     
     }
     
